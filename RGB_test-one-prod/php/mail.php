21854c9deda02451e0abc5332939da1391e5acb0
troloko <troloko8@gmail.com>
2019-07-05 19:18:28 +0300

    изменил путь к fonts

--- RGB_test-one-prod/php/mail.php
+++ RGB_test-one-prod/php/mail.php
@@ -2,8 +2,10 @@
 <?php
 $recepient = "troloko8@gmail.com";
 $siteName = "RGB-test";
+
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
+
 $message = "Имя: $name \nТелефон: $phone";
 $pagetitle = "Заявка с сайта \"$siteName\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
