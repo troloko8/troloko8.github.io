737f995f812f6a6db2d11d4c8fc296d9574395c0
troloko <troloko8@gmail.com>
2019-07-05 19:07:19 +0300

    добавил тестовой задание

---
+++ RGB_test-one-prod/php/mail.php
@@ -0,0 +1,11 @@
+    
+<?php
+$recepient = "troloko8@gmail.com";
+$siteName = "RGB-test";
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone";
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
+
