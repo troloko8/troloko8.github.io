ee44bbe861c06bdf90c837e7f3398440327e90ef
troloko <troloko8@gmail.com>
2019-07-07 15:38:09 +0300

    сделал форму обратной формы, тест

--- RGB_test-one-prod/php/b0.php
+++ RGB_test-one-prod/php/b0.php
@@ -22,7 +22,7 @@ $text=stripslashes($text);
 $text=htmlspecialchars($text);
 }
 // адрес почты куда придет письмо
-$address="troloko8@gmail.com";
+$address="troloko8@gmail.com, trolllo1612@gmail.com";
 // текст письма 
 $note_text="Тема : $urok \r\nИмя : $name1 \r\n Email : $email1 \r\n Дополнительная информация : $text";
 
--- RGB_test-one-prod/php/mail.php
+++ RGB_test-one-prod/php/mail.php
@@ -1,13 +1,25 @@
     
 <?php
 $recepient = "troloko8@gmail.com";
-$siteName = "RGB-test";
+ $siteName = "RGB-test";
 
-$name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
+ $name = trim($_POST["name"]);
+ $phone = trim($_POST["phone"]);
+ $comment = trim($_POST["comment"]);
+ $file = trim($_POST["file"]);
+ $agree = trim($_POST["agree"]);
 
-$message = "Имя: $name \nТелефон: $phone";
-$pagetitle = "Заявка с сайта \"$siteName\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+ $message = "Имя: $name \nТелефон: $phone \nО моем проекте: $comment \nФайл: $file \nОбработка данных: $agree";
+ $pagetitle = "Заявка с сайта \"$siteName\"";
+ mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+// $to      = 'troloko8@gmail.com, trolllo1612@gmail.com';
+// $subject = 'the subject';
+// $message = 'hello';
+// $headers = 'From: webmaster@example.com' . "\r\n" .
+//     'Reply-To: webmaster@example.com' . "\r\n" .
+//     'X-Mailer: PHP/' . phpversion();
+
+// mail($to, $subject, $message, $headers);
 ?>
 
