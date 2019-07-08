f13d2927694dc7e4c305c715fd3740ea8b65eb29
troloko <troloko8@gmail.com>
2019-07-08 19:16:50 +0300

    доделал rgb сайт

--- RGB_test-one-prod/php/mail.php
+++ RGB_test-one-prod/php/mail.php
@@ -1,6 +1,6 @@
     
 <?php
-$recepient = "troloko8@gmail.com";
+$recepient = "frontend@rgb-agency.com.ua";
  $siteName = "RGB-test";
 
  $name = trim($_POST["name"]);
@@ -12,14 +12,5 @@ $recepient = "troloko8@gmail.com";
  $message = "Имя: $name \nТелефон: $phone \nО моем проекте: $comment \nФайл: $file \nОбработка данных: $agree";
  $pagetitle = "Заявка с сайта \"$siteName\"";
  mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
-
-// $to      = 'troloko8@gmail.com, trolllo1612@gmail.com';
-// $subject = 'the subject';
-// $message = 'hello';
-// $headers = 'From: webmaster@example.com' . "\r\n" .
-//     'Reply-To: webmaster@example.com' . "\r\n" .
-//     'X-Mailer: PHP/' . phpversion();
-
-// mail($to, $subject, $message, $headers);
 ?>
 
