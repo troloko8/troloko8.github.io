e3935cebc42f81fd7180690ce9e76191fb858ab6
troloko <troloko8@gmail.com>
2020-05-16 12:34:44 +0300

    add Algo website(last version)

--- AwesomeAI/php/mail.php
+++ AwesomeAI/php/mail.php
@@ -7,10 +7,10 @@ $recepient = "algowavemail@gmail.com";
  $surname = trim($_POST["surname"]);
  $email = trim($_POST["email"]);
  $phone = trim($_POST["phone"]);
- $data = trim($_POST["data&time"]);
+//  $data = trim($_POST["data&time"]);
 
 
- $message = "Имя: $name \nФамилия: $surname \nemail: $email \nТелефон: $phone \nДата и время: $data";
+ $message = "Имя: $name \nФамилия: $surname \nemail: $email \nТелефон: $phone";
  $pagetitle = "Заявка с сайта \"$siteName\"";
  mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 ?>
